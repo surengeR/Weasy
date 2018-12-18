5c4466a95653046d8f4a7936d954ae4029c29885
surengeR <40957246+surengeR@users.noreply.github.com>
2018-12-18 19:59:24 +0300

    Chigidin AI-161

---
+++ main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Calc
+//
+//  Created by Student on 03.10.18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
